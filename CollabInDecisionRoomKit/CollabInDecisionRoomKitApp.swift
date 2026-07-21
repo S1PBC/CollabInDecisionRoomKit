8abf09db6796fbc3ade70361628476f8b37ae662
Logan L <logan@s1pbc.com>
2026-07-21 09:09:54 -0400

    Initial Commit

---
+++ CollabInDecisionRoomKit/CollabInDecisionRoomKitApp.swift
@@ -0,0 +1,40 @@
+//
+//  CollabInDecisionRoomKitApp.swift
+//  CollabInDecisionRoomKit
+//
+//  Created by Logan Lechuga on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct CollabInDecisionRoomKitApp: App {
+    
+    @State private var appModel = AppModel()
+    @State private var avPlayerViewModel = AVPlayerViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            if avPlayerViewModel.isPlaying {
+                AVPlayerView(viewModel: avPlayerViewModel)
+            } else {
+                ContentView()
+                    .environment(appModel)
+            }
+        }
+        
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                    avPlayerViewModel.play()
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                    avPlayerViewModel.reset()
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
